27d93372b8b5980b8c3c625c11cbc168b15f98c8
xrangerbco275 <xrangerbco275@att.net>
2019-12-04 20:04:17 -0600

    initial commit

---
+++ src/main/java/com/jcruz/angularapp/AngularappApplication.java
@@ -0,0 +1,15 @@
+package com.jcruz.angularapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularappApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(AngularappApplication.class, args);
+    }
+
+}
